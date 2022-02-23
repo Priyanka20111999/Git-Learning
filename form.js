545af57f995c8b1e8da87270c017a9332e348259
Priyanka <priyankanayak20111999@gmail.com>
2022-02-23 16:58:22 +0530

    firstweb

---
+++ form.js
@@ -0,0 +1,32 @@
+console.log("hello");
+
+function fun(){
+
+    let name=document.getElementById("name").value;
+console.log(name);
+
+
+
+}
+// function incre(){
+//     let val=document.getElementById("value").innerText;
+//     console.log(typeof(val))
+//     val=parseInt(val)
+//     // console.log(typeof(val))
+//     val=val+1;
+//     document.getElementById("value").innerText=val;
+//     document.getElementById("had").innerText="you increase";
+
+
+// }
+
+// function drcre(){
+    // let val=document.getElementById("value").innerText;
+    // val=parseInt(val)
+    // val=val-1;
+    // document.getElementById("value").innerText=val;
+
+    // document.getElementById("had").innerText="you decrease";
+
+    
+// }
